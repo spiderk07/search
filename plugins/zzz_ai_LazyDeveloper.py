2e9bad153d3ad7eab45bf96b3350c22389d700c3
spiderk07 <130534772+spiderk07@users.noreply.github.com>
2023-12-31 18:29:09 +0530

    Update zzz_ai_LazyDeveloper.py

--- plugins/zzz_ai_LazyDeveloper.py
+++ plugins/zzz_ai_LazyDeveloper.py
@@ -6,14 +6,14 @@ from client import User
 from pyrogram import Client, filters 
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
-@Client.on_message(filters.private & filters.text & filters.incoming)
+@Client.on_message(filters.private & filters.text)
 async def Search(client, message):
-    content = message.text
+    query   = message.text
     user = message.from_user.first_name
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text(
+    await msg.reply_text(
          text=f"<b>ʜᴇʏ {user} 😍 ,\n\nʏᴏᴜ ᴄᴀɴ'ᴛ ɢᴇᴛ ᴍᴏᴠɪᴇs ꜰʀᴏᴍ ʜᴇʀᴇ. ʀᴇǫᴜᴇsᴛ ɪᴛ ɪɴ ᴏᴜʀ <a href=https://t.me/+_AWkWy0499dlZjQ1>Fʀᴇᴇ Mᴏᴠɪᴇ Gʀᴏᴜᴘ</a> ᴏʀ ɢᴇᴛ ᴘʀᴇᴍɪᴜᴍ ᴍᴇᴍʙᴇʀꜱʜɪᴘ 👇</b>",   
          reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("👑 Gᴇᴛ Pʀᴇᴍɪᴜᴍ Mᴇᴍʙᴇʀꜱʜɪᴘ 🫅", url=f"https://t.me/SKadminrobot")]])
     )
